f2715566154e6df82bfc9364d090862f21cf3a73
realkth <miamirocky@gmail.com>
2017-11-01 01:41:47 +0900

    favicon 제작 후 추가

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,14 +9,6 @@ class Navbar extends Component {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-          </ul>
         </div>
       </nav>
     );
